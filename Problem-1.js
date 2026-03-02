5e6ba44b4d7efc40852c12b725dd7219cc118f65
Your Name <you@example.com>
2026-03-02 21:34:10 +0600

    Assignment of JanvaScript Calculation using function

---
+++ Problem-1.js
@@ -0,0 +1,30 @@
+// calculateZooMn
+
+let TotalIncome = 0;
+let totolExpense = 0;
+const everySingleTicketPrice = 120;
+const everySingleStaffLaunch = 50;
+const staff =8;
+const guardSalary = 500;
+let staffLaunchTotalCost=0;
+let dailyTotalCost=0;
+
+function calculateMoney(ticket) {
+
+    if(ticket<=0){
+        return "Today No ticket sale. and faces lose lot's of amount of this zoo authority."
+    }
+    else{
+        TotalIncome = everySingleTicketPrice*ticket;
+        staffLaunchTotalCost= staff * everySingleStaffLaunch;
+        dailyTotalCost=guardSalary+staffLaunchTotalCost;
+
+         let dailyIncome=TotalIncome-dailyTotalCost;
+        return dailyIncome;            
+
+    }
+   
+}
+const saleTicket = -130;
+const totalIncome = calculateMoney(saleTicket);
+console.log(totalIncome);
